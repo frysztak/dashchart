2f95def1abb3260d6d8a61fb2683241f98779116
Sebastian Frysztak <sebastian.frysztak@gmail.com>
2020-07-01 17:48:36 +0200

    FE: add pipe() for nicer composition of webpack configs, add polyfills (unused right now)

--- src/client/next.config.js
+++ src/client/next.config.js
@@ -2,4 +2,29 @@ const withFonts = require('next-fonts');
 const withImages = require('next-images');
 const withTM = require('next-transpile-modules')(['shared', 'fp-ts']);
 
-module.exports = withImages(withFonts(withTM()));
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+
+const withPolyfills = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      const originalEntry = config.entry;
+      config.entry = async () => {
+        const entries = await originalEntry();
+
+        if (entries['main.js'] && !entries['main.js'].includes('./polyfills.js')) {
+          entries['main.js'].unshift('./polyfills.js');
+        }
+
+        return entries;
+      };
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options);
+      }
+
+      return config;
+    },
+  });
+};
+
+module.exports = pipe(withImages, withFonts, withTM)();
