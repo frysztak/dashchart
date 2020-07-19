aef9cdf3d5e42b971e678c9ee63ce75ddff633e6
Sebastian Frysztak <sebastian.frysztak@gmail.com>
2020-07-19 15:38:43 +0200

    BE: implement stats endpoint

--- src/client/pages/api/project/[projectId]/stats.ts
+++ src/client/pages/api/project/[projectId]/stats.ts
@@ -1,9 +1,43 @@
 import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { parseNumericParameter } from '../../../../utils/APIRouterParsers';
 
 const prisma = new PrismaClient();
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-  const projects = await prisma.project.findMany();
-  return res.json(projects);
+  const projectId: number | Error = parseNumericParameter(req, 'projectId');
+  if (projectId instanceof Error) {
+    res.status(500).send(projectId.message);
+    return;
+  }
+  try {
+    const [project, dataFrameCount, chartCount] = await Promise.all([
+      prisma.project.findOne({
+        where: {
+          id: projectId,
+        },
+      }),
+      prisma.dataFrame.count({
+        where: {
+          projectId: projectId,
+        },
+      }),
+      prisma.chart.count({
+        where: {
+          projectId: projectId,
+        },
+      }),
+    ]);
+
+    if (!project) {
+      return res.status(500).send(`ProjectId ${projectId} not found`);
+    }
+    return res.send({
+      ...project,
+      dataFrameCount,
+      chartCount,
+    });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
